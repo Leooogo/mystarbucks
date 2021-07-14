8a767a032e5b8dd6d7f31f2382ab1c1b6a74063e
Minjeong Jang <minjeongg24@gmail.com>
2021-07-14 21:40:43 +0100

    Add rating to Review

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  def new
+    @drink = Drink.find(params[:drink_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @drink = Drink.find(params[:drink_id])
+    @review.drink = @drink
+    if @review.save
+      redirect_to drink_path(@drink)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to drink_path(@review.drink)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
+end
