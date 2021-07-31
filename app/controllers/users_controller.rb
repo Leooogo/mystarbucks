a3bcdc74acb39512fc3418bdc0b36ff6dfade3f6
Minjeong Jang <minjeongg24@gmail.com>
2021-07-31 15:58:22 +0100

    made user page roughly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
+
+  def show
+    if params[:id]
+      user = User.find(params[:id])
+    else
+      user = current_user
+    end
+  end
+
   def edit
     # @user = User.find(params[:id], :conditions => ['opened_once => ?', false]) rescue ActiveRecord::RecordNotFound
     # @user.update_attribute :opened_once, true
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [ :new, :create]
   end
   resources :reviews, only: [ :destroy ]
+  resources :users, only: [ :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
