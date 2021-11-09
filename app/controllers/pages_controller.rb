a43f5bf23609cdad0b216d61303dd0994a33fae3
Minjeong Jang <minjeongg24@gmail.com>
2021-11-09 17:33:06 +0000

    still struggling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,9 @@ class PagesController < ApplicationController
 
   def home
     @drinks = Drink.all
+
+    @ratings = @drinks.each {|drink| drink.reviews.average(:rating).to_f}
+    @ranked_ratings = @ratings.sort_by {|rating| rating}
+    @top_seven = @ranked_ratings.reverse[0..7]
   end
 end
